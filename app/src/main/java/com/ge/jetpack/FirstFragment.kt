6d75db0428e924f4139a611d75ffe36959351acf
melikeertekin <melike.ertekin@geappliances.com>
2020-02-25 13:22:06 -0500

    Add JetPack navigation

---
+++ app/src/main/java/com/ge/jetpack/FirstFragment.kt
@@ -0,0 +1,38 @@
+package com.ge.jetpack
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_first.*
+
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class FirstFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        first_button.setOnClickListener {
+
+        val action = FirstFragmentDirections.actionFirstFragmentToSecondFragment()
+        Navigation.findNavController(it).navigate(action)
+        }
+    }
+
+}
---
+++ app/src/main/java/com/ge/jetpack/SecondFragment.kt
@@ -0,0 +1,37 @@
+package com.ge.jetpack
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_second.*
+
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class SecondFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        second_button.setOnClickListener {
+
+            val action = SecondFragmentDirections.actionSecondFragmentToFirstFragment()
+            Navigation.findNavController(it).navigate(action)
+        }
+    }
+
+}
